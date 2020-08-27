ada0202c58e7c70f7ec10b3b99ef0847ca675424
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-27 11:07:48 -0600

    GET request to json-server

--- views/Inicio.js
+++ views/Inicio.js
@@ -1,7 +1,33 @@
-import React from 'react';
-import {View, StyleSheet, Text,} from 'react-native';
+import React,{useEffect, useState} from 'react';
+import {View, StyleSheet, Text, Platform} from 'react-native';
+import axios from 'axios';
 
 const Inicio = () => {
+
+    const [clientes, setClientes] = useState(false);
+
+    useEffect(() => {
+        const obtenerClientesApi = async () => {
+            try {
+                if(Platform.OS === 'ios'){
+                    //Para ios
+                   const response = await axios.get('http://localhost:3000/clientes');
+                   setClientes(response.data);
+                }else{
+                   // para android
+                   const response = await axios.get('http://10.0.2.2:3000/clientes');
+                   setClientes(response.data);
+                }
+            } catch (error) {
+                console.error;
+            }
+        }
+
+        obtenerClientesApi();
+    },[]);
+
+    if(!clientes ) return null;
+
     return ( 
         <>
         <Text>Inicio</Text>
