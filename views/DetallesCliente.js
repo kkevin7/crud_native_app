f8e9b015a3d65154fb130e989a574b41f32f84c9
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-27 11:47:58 -0600

    show clients details data

--- views/DetallesCliente.js
+++ views/DetallesCliente.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import {View, StyleSheet, Text,} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {Headline, Text, Subheading, Button} from 'react-native-paper';
+import globalStyles from '../styles/global';
 
 const DetallesCliente = ({route}) => {
-    const {item } = route.params;
+    const {nombre, telefono, correo, empresa} = route.params.item;
     return ( 
         <>
-        <Text>DetallesCliente</Text>
+        <View style= {globalStyles.contenedor}>
+            <Headline style={globalStyles.titulo}>{nombre}</Headline>
+            <Text style={styles.texto}>Empresa: <Subheading>{empresa}</Subheading></Text>
+            <Text style={styles.texto}>Teléfono: <Subheading>{telefono}</Subheading></Text>
+            <Text style={styles.texto}>Correo: <Subheading>{correo}</Subheading></Text>
+
+            <Button>
+                Eliminar Cliente
+            </Button>
+        </View>
         </>
      );
 }
+
+const styles = StyleSheet.create({
+    texto: {
+        marginBottom: 20,
+        fontSize: 18,
+    }
+})
  
 export default DetallesCliente;
